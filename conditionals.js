4676d4793d6554969745b02296f4d6fa1edae1a8
cchand-vanc <cmchandler@live.co.uk>
2022-12-14 16:01:09 -0800

    Example of using a function in a conditional statement

--- conditionals.js
+++ conditionals.js
@@ -1,34 +1,44 @@
-const raining = true;
-const cold = true;
+// const raining = true;
+// const cold = true;
 
-if (raining) {
-  console.log ("Don't forget your umbrella!");
-}
+// if (raining) {
+//   console.log ("Don't forget your umbrella!");
+// }
 
-if (cold) {
-  console.log ("Make sure you pick out a scarf!");
-}
+// if (cold) {
+//   console.log ("Make sure you pick out a scarf!");
+// }
 
-console.log("Now you're ready to go outside!");
-
-
-const temperature = 12;
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-console.log("Now you're ready to go outside!");
+// console.log("Now you're ready to go outside!");
 
 
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea...");
-}
+// const temperature = 12;
 
-if (!raining) {
-  console.log ("Leave your umbrella at home!");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea...");
+// }
+
+// if (!raining) {
+//   console.log ("Leave your umbrella at home!");
+// }
+
+const whichSchool = function(age) {
+  if (age < 13) {
+    return "Elementary school";
+  } else if (age >= 14 && age <= 18) {
+    return "Secondary school";
+  } else {
+    return "Lighthouse Labs";
+  }
 }
