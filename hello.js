b5ac7e0da806128630b2e475fdedff0ae3746e4a
cchand-vanc <cmchandler@live.co.uk>
2022-12-15 19:11:41 -0800

    basic function playing aroun

--- hello.js
+++ hello.js
@@ -1,5 +1,10 @@
-const sayHello = function(name){
-  console.log("Hello " + name);
-}
+// const sayHello = function(name){
+//   console.log("Hello " + name);
+// }
 
-sayHello("Charlie");
+// sayHello("Charlie");
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
